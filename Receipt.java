43e0b306e87420758c6e1afb733db3bced1f9e6a
unknown <yllsulejmani92@outlook.com>
2020-04-12 20:10:22 +0200

    new changes

---
+++ Receipt.java
@@ -0,0 +1,11 @@
+import java.util.*;  
+ class Receipt{  
+ public static void main(String args[]){  
+List<String> list=new List<String>();   
+      list.add("rakija");
+      list.add("vodka");    
+         
+     
+  }  
+ }  
+}  
